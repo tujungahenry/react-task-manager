886ccb2ee0f4b38081ebb31ec47f5d0526ced857
Henry Tum <tujungahenry@gmail.com>
2020-04-22 21:47:19 -0700

    add coments and styling

--- task-manager/src/contexts/TaskListContext.js
+++ task-manager/src/contexts/TaskListContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useState, useEffect } from "react";
-import TaskList from "../components/TaskList";
 import uuid from "uuid/v4";
 
 export const TaskListContext = createContext();
@@ -9,30 +8,35 @@ const TaskListContextProvider = (props) => {
 
   const [tasks, setTasks] = useState(initialState);
 
-  const [editItem, setEditItem] = useState(null);
-
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   }, [tasks]);
 
+  const [editItem, setEditItem] = useState(null);
+
+  // Add tasks
   const addTask = (title) => {
     setTasks([...tasks, { title, id: uuid() }]);
   };
 
+  // Remove tasks
   const removeTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Clear tasks
   const clearList = () => {
     setTasks([]);
   };
 
+  // Find task
   const findItem = (id) => {
     const item = tasks.find((task) => task.id === id);
 
     setEditItem(item);
   };
 
+  // Edit task
   const editTask = (title, id) => {
     const newTasks = tasks.map((task) =>
       task.id === id ? { title, id } : task
